8f7ac47d9c2f65aa2c321140962fcf390975e79d
Zheka Chakhov <zhekachakhov@mail.ru>
2023-09-15 16:59:33 +0300

    edit layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,79 +1,17 @@
 <template>
   <div>
-    <div class="grid grid-cols-4 gap-5">
-      <div v-for="ch of characters">
-        <CharacterCard :character="ch" />
-      </div>
-    </div>
-  </div>
-  <div class="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-    <div>
-      <nav v-for="i in arr" 
-      class="isolate inline-flex -space-x-px rounded-md shadow-sm"
-      aria-label="Pagination">
-        <NuxtLink
-          :to="`${i}`"
-          href="#"
-          :class="['regular', { active: i === 1 }]">
-          {{i}}
-        </NuxtLink>
-      </nav>
-    </div>
+    <h2>Home</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In perspiciatis omnis consequatur quod necessitatibus autem repellat aperiam quia atque fugit praesentium earum rerum minus, porro asperiores explicabo inventore animi ducimus. Itaque voluptatum facilis nobis, praesentium, quasi pariatur veniam veritatis quo eligendi, ratione libero impedit. Distinctio, delectus sapiente adipisci assumenda obcaecati aliquid exercitationem sit ad autem! Veritatis nihil accusamus recusandae esse?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In perspiciatis omnis consequatur quod necessitatibus autem repellat aperiam quia atque fugit praesentium earum rerum minus, porro asperiores explicabo inventore animi ducimus. Itaque voluptatum facilis nobis, praesentium, quasi pariatur veniam veritatis quo eligendi, ratione libero impedit. Distinctio, delectus sapiente adipisci assumenda obcaecati aliquid exercitationem sit ad autem! Veritatis nihil accusamus recusandae esse?</p>
   </div>
 </template>
 
-<script setup>
-import { onMounted, computed } from "vue";
-import { useCharacterStore } from "~/stores/CharacterStore";
-
-const arr = [];
-
-for (let j = 1; j < 43; j++) {
-  arr.push(j)
-}
-
-const characterStore = useCharacterStore();
-
-const characters = computed(() => {
-  return characterStore.characters;
-});
-
-onMounted(() => {
-  characterStore.fetchCharacters();
-});
-
-</script>
-
 <style scoped>
-.regular {
-  display: inline-flex; 
-  position: relative; 
-  padding-top: 0.5rem;
-  padding-bottom: 0.5rem; 
-  padding-left: 1rem;
-  padding-right: 1rem; 
-  align-items: center; 
-  box-shadow: var(--tw-ring-inset) 0 0 0 calc(1px + var(--tw-ring-offset-width)) var(--tw-ring-color); 
-  --tw-ring-inset: inset; 
-  --ring-color: #D1D5DB; 
-  font-size: 0.875rem;
-  line-height: 1.25rem; 
-  font-weight: 600; 
-  color: #111827; 
-}
-.regular:hover {
- background-color: #F9FAFB; 
- }
-
- .active {
-  z-index: 10; 
-  box-shadow: var(--tw-ring-inset) 0 0 0 calc(1px + var(--tw-ring-offset-width)) var(--tw-ring-color); 
-  --tw-ring-inset: inset; 
-  --ring-color: #D1D5DB; 
-  color: #ffffff; 
-  background-color: #4F46E5;
- }
-  .active:hover {
-  background-color: #F9FAFB; 
- }
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  p {
+    margin: 20px 0;
+  }
 </style>
